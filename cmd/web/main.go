1bd05c39c6a9167961d360de3fa1469795291ee3
rosswf <ross@rossw.co.uk>
2023-02-01 18:49:36 +0000

    Serve homepage

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/rosswf/dabogo/internal/models"
 )
 
 type application struct {
 	config *models.Config
+}
+
+func main() {
+	f, err := os.Open("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := models.NewConfig()
+	cfg.Load(f)
+
+	app := application{
+		config: cfg,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+
+	http.ListenAndServe(":8080", mux)
 }
