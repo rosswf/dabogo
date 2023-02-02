696898419f8b1bf8e713be56916fddedba6a0cef
rosswf <ross@rossw.co.uk>
2023-02-02 13:32:24 +0000

    Allow custom colors for each link

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -73,3 +73,56 @@ func TestHomeHandler(t *testing.T) {
 		t.Errorf("got: %s, want: %s", got, want)
 	}
 }
+
+func TestHomeHandlerCustomColor(t *testing.T) {
+	template, err := newTemplate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app := application{
+		config: &models.Config{
+			Links: []models.Link{
+				{
+					Name:  "Google",
+					Url:   "https://google.co.uk",
+					Color: "#ffffff",
+				},
+				{
+					Name: "BBC",
+					Url:  "https://bbc.co.uk",
+				},
+			},
+		},
+		template: template,
+	}
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app.home(w, r)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	// Check that the status code is 200.
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("got: %d, want: %d", res.StatusCode, http.StatusOK)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check that the response body contains "#ffffff"
+	got := string(body)
+	want := "#ffffff"
+
+	if !strings.Contains(got, want) {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
